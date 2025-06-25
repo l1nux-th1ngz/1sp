31c6ed04a9cf8ce388e01ab76d3b1f25e8dfd17e
l1nux-th1ngz <137152321+l1nux-th1ngz@users.noreply.github.com>
2025-06-25 04:01:18 -0600

    Update jq.sh

--- jq.sh
+++ jq.sh
@@ -1,16 +1,7 @@
 #!/bin/bash
 
-# Download jq binary
-wget -q -O jq-linux-amd64 https://github.com/jqlang/jq/releases/download/jq-1.8.0/jq-linux-amd64
-
-# Move to /usr/local/bin  
-sudo mv jq-linux-amd64 /usr/local/bin
-
-# Set permissions
+wget -q -O jq /https://github.com/jqlang/jq/releases/download/jq-1.8.0/jq-linux-amd64
+sudo mv jq /usr/local/bin
 sudo chmod +x /usr/local/bin/jq
 
-# Set JQ_COLORS environment variable for truecolor support
 export JQ_COLORS="38;2;255;173;173:38;2;255;214;165:38;2;253;255;182:38;2;202;255;191:38;2;155;246;255:38;2;160;196;255:38;2;189;178;255:38;2;255;198;255"
-
-# Test jq with colors
-jq -nc '[null,false,true,42,{"a":"bc"}]'
